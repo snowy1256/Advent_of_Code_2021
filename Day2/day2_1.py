1a90f8c07b82d380096ba2b4747c48c9c9933ee4
Dominic Noble <dominic.noble@ubisoft.com>
2021-12-02 13:36:02 +0000

    add day2 part 1

---
+++ Day2/day2_1.py
@@ -0,0 +1,27 @@
+INPUT_ARRAY = []
+
+def parse_input():
+    with open("day2_input.txt") as f:
+        for input in f:
+            instruction = input.split(" ")
+            INPUT_ARRAY.append(instruction)
+
+
+def steer_submarine():
+    submarine_position = [0, 0]
+    for direction, distance in INPUT_ARRAY:
+        if direction == "forward":
+            submarine_position[0] += int(distance)
+        if direction == "down":
+            submarine_position[1] += int(distance)
+        if direction == "up":
+            submarine_position[1] -= int(distance)
+    return submarine_position
+
+def mult_position(sub_pos):
+    print(sub_pos[0] * sub_pos[1])
+
+if __name__ == "__main__":
+    parse_input()
+    sub_pos = steer_submarine()
+    mult_position(sub_pos)
